f128f2186cd76353c5a0ed41ed702b4b6802461c
unknown <rojewski@webkon.eu>
2022-11-16 20:27:31 +0100

    Updates

---
+++ precache-manifest.9c81781f01f22fc77d5fbf57e884b73e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "538c6125af9972fbb69a",
+    "url": "/rr2022/static/css/main.3028d335.chunk.css"
+  },
+  {
+    "revision": "538c6125af9972fbb69a",
+    "url": "/rr2022/static/js/main.538c6125.chunk.js"
+  },
+  {
+    "revision": "22d46195bcc7a81a32eb",
+    "url": "/rr2022/static/js/runtime~main.22d46195.js"
+  },
+  {
+    "revision": "328dd81371343b01b831",
+    "url": "/rr2022/static/js/2.328dd813.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/rr2022/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/rr2022/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/rr2022/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "fb9285430107c1dc4f4d15e0624340dd",
+    "url": "/rr2022/index.html"
+  }
+];
